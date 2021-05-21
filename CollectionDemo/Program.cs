75605489008d5625f2ad67ab422d529d89f3d63e
shashiRgowda <shashikumartr64@gmail.com>
2021-05-21 19:52:00 +0530

    Added:CollectionDemo

---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Collections");
+            doSetDemo();
+            doListDemo();
+            doQueueDemo();
+            doDictionaryDemo();
+            doStackDemo();
+            doListDemo();
+        }
+
+        public static void doSetDemo()
+        {
+            Console.WriteLine("\nIn doSetDemo");
+            var set = new HashSet<string>();
+            set.Add("Ravi");
+            set.Add("Vijay");
+            set.Add("Ravi");
+            set.Add("Ajay");
+            HashSet<string>.Enumerator enumerator = set.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+
+        }
+
+        private static void doQueueDemo()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Amit Sharma");
+            queue.Enqueue("Vijay Raj");
+            queue.Enqueue("Jaishankar");
+            queue.Enqueue("Raj");
+            queue.Enqueue("Raj");
+            Console.WriteLine("Haed:" + queue.Peek());
+            Console.WriteLine("\n Iterating the queue elements:");
+            foreach (var element in queue)
+            {
+                Console.WriteLine(element);
+            }
+            string dequeue = queue.Dequeue();
+            Console.WriteLine("Dequeue element:" + dequeue);
+            Console.WriteLine("\nIterating queue elements after dequeue one element:");
+            Queue<string>.Enumerator enumerator = queue.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+        }
+        private static void doDictionaryDemo()
+        {
+            Console.WriteLine("\nIn doStackDemo");
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(100, "Amit");
+            dictionary.Add(101, "Rahul");
+            dictionary.Add(102, "Vijya");
+            Console.WriteLine("Access value using key(key=100):" + dictionary[100]);
+            Console.WriteLine("\nItearating Dictionary:");
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine("key=" + element.Key + "Value=" + element.Value);
+            }
+        }
+        private static void doStackDemo()
+        {
+            Console.WriteLine("\nIn doStackDemo");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Ayush");
+            stack.Push("Amit");
+            stack.Push("Ashish");
+            stack.Push("Garina");
+            string pop = stack.Pop();
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("Popped element:" + pop);
+        }
+        private static void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDemo");
+            List<string> list = new List<string>();
+            list.Add("Ravi");
+            list.Add("Vijay");
+            list.Add("Ravi");
+            list.Add("Ajay");
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(list[2]);
+        }
+    }
+}
